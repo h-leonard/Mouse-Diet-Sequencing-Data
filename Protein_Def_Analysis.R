0263d61e137cd8404986cf4748596b238a5b081f
Hampton Leonard <hll4ce@virginia.edu>
2017-08-11 14:32:40 -0400

    added file

---
+++ Protein_Def_Analysis.R
@@ -0,0 +1,87 @@
+## Protein Deficient
+
+
+
+source("https://bioconductor.org/biocLite.R")
+library(dada2)
+library(ggplot2)
+library(DESeq2)
+library(phyloseq)
+library(randomForest)
+library(dplyr)
+library(knitr)
+
+
+
+
+sample_df_protein <- data.frame(sample=samples.out, description=diet_description, day=days_on_diet)
+rownames(sample_df_protein) <- samples.out
+
+
+
+# remove antibiotic time point
+sample_df_protein <- sample_df_protein[sample_df_protein$description != "dPD, 23 days, post_Abx",]
+
+
+sample_df_protein <- sample_df_protein[!grepl("dZD", sample_df_protein$description),]
+
+sample_df_protein <- sample_df_protein[!grepl("dN", sample_df_protein$description),]
+
+
+
+ps_protein <- phyloseq(otu_table(seqtab.nochim.taxa, taxa_are_rows=FALSE), 
+                    sample_data(sample_df_protein), 
+                    tax_table(taxa.table))
+
+
+
+# Filter sequences that have less than 10 counts in any sample
+ps.count_protein <- filter_taxa(ps_protein, function(x) max(x) > 10, TRUE)
+ps.count_protein
+
+
+
+#convert to factor for DESeq
+sample_data(ps.count_protein)$description <- factor(sample_data(ps.count_protein)$description)
+
+
+
+# Create separate object for comparisons at each time point
+d0_subset_pro <- prune_samples(sample_data(ps.count_protein)$description == "dPD, weaned", ps.count_protein)
+d5_subset_pro <- prune_samples(sample_data(ps.count_protein)$description == "dPD, 5 days", ps.count_protein)
+d8_subset_pro <- prune_samples(sample_data(ps.count_protein)$description == "dPD, 8 days", ps.count_protein)
+d12_subset_pro <- prune_samples(sample_data(ps.count_protein)$description == "dPD, 12 days", ps.count_protein)
+d15_subset_pro <- prune_samples(sample_data(ps.count_protein)$description == "dPD, 15 days", ps.count_protein)
+
+
+
+
+# Day 0 comparison ############
+deseq2_input_d0 <- phyloseq_to_deseq2(ps.count_protein, ~description)
+deseq2_output_d0 <- DESeq(deseq2_input_d0, test="Wald", fitType="parametric")
+deseq2_results_d0 <- results(deseq2_output_d0, cooksCutoff = FALSE)
+
+alpha = 0.05
+sigtab_d0 = deseq2_results_d0[which(deseq2_results_d0$padj < alpha), ]
+sigtab_d0 = cbind(as(sigtab_d0, "data.frame"), as(tax_table(ps.count)[rownames(sigtab_d0), ], "matrix"))
+sigtab_d0
+
+
+
+
+seqs <- rownames(sigtab_d0)
+
+# Transform sequence table from counts to relative abundance
+ps.rel <- transform_sample_counts(ps.count_protein, function(x) x/sum(x))
+ps.rel
+
+
+p <- plot_heatmap(prune_taxa(seqs,ps.rel),"NMDS","bray",sample.label = "description","Genus", first.sample = "Plate3-A2")
+p + theme (axis.text.x = element_text(size=6))
+
+
+
+
+
+
+
